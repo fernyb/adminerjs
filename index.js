eb93c994de81ebe86fdb4b06ba50f00dd394d0d9
Fernando Barajas <fernyb@fernyb.net>
2021-11-27 19:01:09 -0800

    some more

--- index.js
+++ index.js
@@ -3,42 +3,71 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const FormData = require("form-data");
 
-const url = "https://demo.adminer.org/adminer.php?username=&db=a264133_13fcqrod&sql=select%20*%20from%20songs%20limit%2050";
+const url = "https://demo.adminer.org/adminer.php?username=&db=a264133_1ho7o13j&sql=";
+
+function cookie() {
+  return "adminer_permanent=; adminer_sid=h2ing8vga2m138g20d1ho7o13j";
+}
+
+function getToken() {
+  return new Promise((resolve) => {
+    axios.create({
+      headers: {
+        "Cookie": cookie()
+      }
+    }).get(url).then((resp) => {
+      const { document } = (new JSDOM(resp.data)).window;
+      const token = document.querySelector("input[name=token]").getAttribute("value");
+      resolve(token);
+    });
+  });
+}
 
 function executeSQL(statement) {
   const form = new FormData();
   form.append("query", statement);
   form.append("limit", "");
-  form.append("token", "895700:573804");
 
-  return new Promise((resolve) => {
-    axios.create({
-      headers: {
-        ...form.getHeaders(),
-        "Cookie": "adminer_permanent=; adminer_key=37392938842a244da90afc89a29a95f7; adminer_sid=ur6qlthh8dl0jujslta95hv048",
-        "cache-control": "max-age=0",
-        "Content-Length": form.getLengthSync(),
-      },
-    }).post(url, form).then((resp) => {
-      const html = resp.data;
-      const { document } = (new JSDOM(html)).window;
-      const table = document.querySelectorAll("table")[0];
-      const columns = table.querySelectorAll("thead th");
-      let columnNames = [];
-      columns.forEach((column) => {
-        columnNames.push(column.textContent);
-      });
+  return new Promise((resolve, reject) => {
+    getToken().then((token) => {
+      form.append("token", token);
+    }).then(() => {
+      axios.create({
+        headers: {
+          ...form.getHeaders(),
+          "Cookie": cookie(),
+          "cache-control": "max-age=0",
+          "Content-Length": form.getLengthSync(),
+        },
+      })
+      .post(url, form)
+      .then((resp) => {
+        const html = resp.data;
+        const { document } = (new JSDOM(html)).window;
+        const table = document.querySelectorAll("table")[0];
+        const columns = table.querySelectorAll("thead th");
+        let columnNames = [];
+        columns.forEach((column) => {
+          columnNames.push(column.textContent);
+        });
 
-      const rows = [];
-      table.querySelectorAll("tbody > tr").forEach((trNode) => {
-        const tdNodes = trNode.querySelectorAll("td");
-        const dataRow = {};
-        for(let i=0; i<columnNames.length; i++) {
-          dataRow[columnNames[i]] = tdNodes[i].textContent;
-        };
-        rows.push(dataRow);
+        const rows = [];
+        table.querySelectorAll("tbody > tr").forEach((trNode) => {
+          const tdNodes = trNode.querySelectorAll("td");
+          const dataRow = {};
+          for(let i=0; i<columnNames.length; i++) {
+            dataRow[columnNames[i]] = tdNodes[i].textContent;
+          };
+          rows.push(dataRow);
+        });
+        resolve(rows);
+      })
+      .catch((err) => {
+        console.log("Error");
+        console.log(err);
+        reject(err.message);
       });
-      resolve(rows);
+      // -- 
     });
   });
 }
--- test/select.spec.js
+++ test/select.spec.js
@@ -1,9 +1,96 @@
 const { sql } = require("../index");
 
+class Base {
+  static tableName = '';
+  static selectedColumns = [];
+
+  static findBySql(statement) {
+    this.selectedColumns = [];
+    console.log(statement);
+    return sql(statement);
+  }
+
+  static select(...columns) {
+    this.selectedColumns = columns;
+    return this;
+  }
+
+  static findAll() {
+    return this.findBySql(`select * from \`${this.tableName}\``);
+  }
+
+  static findById(id) {
+    if (typeof id === 'string') {
+      id = parseInt(id, 10);
+    }
+    return this.where({ id });
+  }
+
+  static where(fields={}) {
+    let columns = (this.selectedColumns || []).join(", ");
+    if (columns.length === 0) {
+      columns = "*";
+    }
+
+    const whereStr = Object.entries(fields).map(([field, val]) => {
+      if (Array.isArray(val)) {
+        return `${field} IN(${val.join(",")})`
+      }
+      return `${field}=${val}`;
+      }).join(" AND ");
+    return this.findBySql(`select ${columns} from \`${this.tableName}\` where ${ whereStr }`);
+  }
+}
+
+class Song extends Base {
+  static tableName = 'songs';
+}
+
+class Album extends Base {
+  static tableName = 'albums';
+}
+
 describe("SELECT", () => {
   it("select * from 'songs' limit 2", () => {
-    sql("select * from `songs` order by rank desc limit 2").then((results) => {
-      console.log("SQL RESULTS: ", results);
+    Song.findAll().then((results) => {
+      console.log("----------------------------------------------");
+      console.log("Done");
+      console.log(results);
+    });
+  });
+
+  it("select * from songs where id = 2", () => {
+    Song.findAll().then((songs) => {
+      songs.forEach((song) => {
+        Album.findById(song.album).then((albumResult) => {
+          console.log("Album: ");
+          console.log(albumResult);
+        });
+      });
+    });
+  });
+
+  it("select * from songs where id = 3 and album = 1", () => {
+    Song.where({ id: 3, album: 1 }).then((rows) => {
+      console.log(rows);
+    });
+  });
+
+  it.only("select id, album, title from songs where id = 3 and album = 1", () => {
+    Song.where({ id: 2 }).then((rows) => {
+      console.log(rows);
+    });
+
+    Song.select("id", "album", "title").where({ id: 2 }).then((rows) => {
+      console.log(rows);
+    });
+
+    Song.where({ id: 2 }).then((rows) => {
+      console.log(rows);
+    });
+
+    Song.where({ id: [1, 2, 3] }).then((rows) => {
+      console.log(rows);
     });
   });
 });
